274783df7e1936c298451c69ae263a583165a794
adonai-luque <akluqueprieto@gmail.com>
2021-08-10 07:23:58 -0500

    Completed CRUD operations for Movies with controllers and views

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,16 +1,48 @@
 class MoviesController < ApplicationController
-  before_action :set_movie, only: %i[ show edit update destroy ]
-  
   def index
     @movies = Movie.all
   end
-  
-  private
 
-  def set_movie
+  def show
+    @movie = Movie.find(params[:id])
+  end
+  
+  def new
+    @movie = Movie.new
+  end
+
+  def create
+    @movie = Movie.new(movie_params)
+
+    if @movie.save
+      redirect_to @movie
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
     @movie = Movie.find(params[:id])
   end
 
+  def update
+    @movie = Movie.find(params[:id])
+
+    if @movie.update(movie_params)
+      redirect_to @movie
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @movie = Movie.find(params[:id])
+    @movie.destroy
+
+    redirect_to root_path
+  end
+
+  private
   def movie_params
     params.require(:movie).permit(:id, :name, :director, :category_id, :release_date, :user_id)
   end
