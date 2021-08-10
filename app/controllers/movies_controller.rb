f11103ca57bc21bca9bd1c83f0331abb93c87a04
adonai-luque <akluqueprieto@gmail.com>
2021-08-10 16:19:20 -0500

    Deleted methods needed in the old structure

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,6 +1,4 @@
 class MoviesController < ApplicationController
-  rescue_from ActiveRecord::InvalidForeignKey, with: :invalid_foreign_key
-  
   def index
     @movies = Movie.all
   end
@@ -40,26 +38,6 @@ class MoviesController < ApplicationController
   def destroy
     @movie = Movie.find(params[:id])
     @movie.destroy
-
-    redirect_to root_path
-  end
-
-  def select_movie_user
-    @movie = Movie.find(params[:id])
-  end
-
-  def rent
-    @movie = Movie.find(params[:id])
-    if @movie.rent(params[:user_id])
-      redirect_to movies_path
-    else
-      render :select_movie_user, status: :unprocessable_entity
-    end
-  end
-
-  def unrent
-    @movie = Movie.find(params[:id])
-    @movie.unrent
 
     redirect_to root_path
   end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,6 @@
 class Movie < ApplicationRecord
   belongs_to :category
-  # belongs_to :user, optional: true
+  
   has_many :rents, dependent: :destroy
   has_many :users, through: :rents
   
@@ -8,26 +8,6 @@ class Movie < ApplicationRecord
   validates :director, presence: true
   validates :release_date, presence: true
   
-  # def rent(user_id)
-  #   return if self.rented?
-    
-  #   self.user_id = user_id
-  #   self.save
-  # end
-
-  # def unrent
-  #   return unless self.rented?
-
-  #   self.user_id = nil
-  #   self.save
-  # end
-  
-  # def rented?
-  #   return false unless self.user_id
-    
-  #   true
-  # end
-
   def rented?
     self.users.any?
   end
